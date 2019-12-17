c24d4e526588a5d9534b4647a61b014c28150795
pran <training_C2D.02.11@CDC2-D-6LHDH92.dir.svc.accenture.com>
2019-12-17 15:57:13 +0530

    login

---
+++ NewTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void googletest() {
+	  System.out.println("google");
+  }
+  @Test
+  public void twittertest() {
+	  System.out.println("twitter");
+  }
+
+
+}
